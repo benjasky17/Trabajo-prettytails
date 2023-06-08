2b5a2dd0f530cab023f8ca9589633f987deacc21
Benjamin Guajardo <benja.guajardo0608@gmail.com>
2023-06-08 16:01:34 -0400

    trabajo sin el recuperar contraseña y navbar listo

---
+++ prettytailstrabajo/accounts/signals.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import Group
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from .models import Profile
+
+@receiver(post_save, sender=Profile)
+def add_user_to_users_group(sender, instance, created, **kwargs):
+    if created:
+        try:
+            users = Group.objects.get(name='Cliente')
+        except Group.DoesNotExist:
+            users = Group.objects.create(name='Veterinario')
+            users = Group.objects.create(name='Administrativo')
+        instance.user.groups.add(users)
